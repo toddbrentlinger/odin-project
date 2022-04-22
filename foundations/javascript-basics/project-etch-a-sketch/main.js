5a3765aaf061c8a0129bec8668fed1c9af422c0d
toddbrentlinger <toddbrentlinger@gmail.com>
2022-04-22 16:43:20 -0700

    Refactor etch-a-sketch and improve layout
    Combines multiple etch-a-sketch instances, each with single separate
    functionality into one with combined functionality. Improves darken
    and lighten functions to change existing background-color instead of
    relying on data attributes.
    
    Adds buttons to select different funcionality and color inputs to select
    foreground and background color. Adds multiple background images of
    different sizes to improve performance.

--- foundations/javascript-basics/project-etch-a-sketch/main.js
+++ foundations/javascript-basics/project-etch-a-sketch/main.js
@@ -2,24 +2,27 @@
 
 /**
  * 
- * @param {Element} etchASketchElement
- * @param {Function} handleOnMouseOver 
+ * @param {Element} etchASketchContainerElement
+ * @param {Number} numPerSide 
  */
-function EtchASketch(etchASketchElement, handleOnMouseOver) {
-    this.etchASketchElement = etchASketchElement;
-    this.handleOnMouseOver = handleOnMouseOver;
+function EtchASketch(etchASketchContainerElement, numPerSide = 16) {
+    this.containerElement = etchASketchContainerElement;
+    this.numPerSide = numPerSide;
+    this.selectedFGColor = '#000000';
+    this.selectedBGColor = '#FFFFFF';
+    this.bInteractOnMouseOver = true;
+    this.handleOnMouseOver = this.addSelectedFGColor;
+    this.handleOnMouseDown = null;
 }
 
 /** Initializes an instance of EtchASketch. */
 EtchASketch.prototype.init = function() {
-    // Create 'Clear' button
-    const clearBtn = document.createElement('button');
-    clearBtn.textContent = 'Clear';
-    clearBtn.addEventListener('click', function() {
-        this.handleClearBoard();
-    }.bind(this), false);
-    this.etchASketchElement.appendChild(clearBtn);
-
+    this.inputContainerElement = this.containerElement.querySelector('.etch-a-sketch-input');
+    this.etchASketchElement = this.containerElement.querySelector('.etch-a-sketch');
+    if (!this.etchASketchElement) {
+        // TODO: Create element with class 'etch-a-sket' and append as child to container element
+        // Just in case the element doesn't exist
+    }
     // Create and set board container node to hold created square grid
     this.boardContainerNode = document.createElement('div');
     this.boardContainerNode.classList.add('square-board-container');
@@ -27,26 +30,134 @@ EtchASketch.prototype.init = function() {
 
     // Create sqaure grid with default number of squares on each side
     this.createSquareGrid();
+
+    // Add button event listeners
+    let element;
+
+    // FG Color Picker
+    element = this.inputContainerElement.querySelector('[data-key="fg-color-picker"]');
+    if (element) {
+        element.value = this.selectedFGColor;
+        element.addEventListener('input', function(e) {
+            this.selectedFGColor = e.target.value;
+            this.setActiveFunction(this.addSelectedFGColor);
+        }.bind(this), false);
+    }
+
+    // BG Color Picker
+    element = this.inputContainerElement.querySelector('[data-key="bg-color-picker"]');
+    if (element) {
+        element.value = this.selectedBGColor;
+        element.addEventListener('input', function(e) {
+            this.selectedBGColor = e.target.value;
+        }.bind(this), false);
+    }
+    
+    // Random Color
+    this.setupInputButton('random-color', 'click', this.randomColor);
+    
+    // Erase Color
+    this.setupInputButton('erase-color', 'click', this.erase);
+    
+    // Darken Shade
+    this.setupInputButton('darken', 'click', function(e) {
+        this.shade(e.target, true);
+    }.bind(this));
+    
+    // Lighten Shade
+    this.setupInputButton('lighten', 'click', function(e) {
+        this.shade(e.target, false);
+    }.bind(this));
+    
+    // Clear
+    this.setupInputButton('clear', 'click', this.handleClearBoard);
+    element = this.inputContainerElement.querySelector('[data-key="clear"]');
+    if (element) {
+        element.addEventListener('click', function() {
+            this.handleClearBoard();
+        }.bind(this), false);
+    }
+
+    // Mouse Over
+    this.setupInputButton('mouse-over', 'click', function() {
+        // If mouse-over is already set, return
+        if (this.bInteractOnMouseOver) return;
+        this.bInteractOnMouseOver = true;
+        this.handleOnMouseOver = this.handleOnMouseDown;
+        this.handleOnMouseDown = null;
+
+    }.bind(this));
+
+    // Mouse Down
+    this.setupInputButton('mouse-down', 'click', function() {
+        // If mouse-down is already set, return
+        if (!this.bInteractOnMouseOver) return;
+        this.bInteractOnMouseOver = false;
+        this.handleOnMouseDown = this.handleOnMouseOver;
+        this.handleOnMouseOver = null;
+    }.bind(this));
 };
 
 /**
- * Creates a square grid with equal number of smaller squares along each side.
- * @param {Number} numPerSide
+ * 
+ * @param {String} dataKey 
+ * @param {String} type
+ * @param {Function} listener 
  */
-EtchASketch.prototype.createSquareGrid = function(numPerSide = 16) {
+EtchASketch.prototype.setupInputButton = function(dataKey, type, listener) {
+    const element = this.inputContainerElement.querySelector(`[data-key="${dataKey}"]`);
+    if (element) {
+        element.addEventListener(type, function() {
+            this.setActiveFunction(listener);
+        }.bind(this));
+    }
+};
+
+/**
+ * 
+ * @param {Function} func 
+ */
+EtchASketch.prototype.setActiveFunction = function(func) {
+    if (this.bInteractOnMouseOver) {
+        this.handleOnMouseOver = func;
+        this.handleOnMouseDown = null;
+    } else {
+        this.handleOnMouseOver = null;
+        this.handleOnMouseDown = func;
+    }
+};
+
+/** Creates a square grid with equal number of smaller squares along each side. */
+EtchASketch.prototype.createSquareGrid = function() {
     const boardNode = document.createElement('div');
     boardNode.classList.add('board');
 
     let rowNode, dataNode;
-    for (let i = 0; i < numPerSide; i++) {
+    for (let i = 0; i < this.numPerSide; i++) {
         rowNode = document.createElement('div');
         rowNode.classList.add('board-row');
-        for (let j = 0; j < numPerSide; j++) {
+        for (let j = 0; j < this.numPerSide; j++) {
             dataNode = document.createElement('div');
             dataNode.classList.add('board-square');
+
+            // Add row and column number as data attribute (may not be necessary)
             dataNode.dataset.row = i;
             dataNode.dataset.col = j;
-            dataNode.addEventListener('mouseover', this.handleOnMouseOver, false);
+
+            // Add event listeners for 'mouseover' and 'mousedown' events
+            dataNode.addEventListener('mouseover', function(e) {
+                if (this.handleOnMouseOver)
+                    this.handleOnMouseOver(e);
+            }.bind(this), false);
+
+            dataNode.addEventListener('mousedown', function(e) {
+                console.log(`Mouse Down: r${i} : c${j}`);
+                if (this.handleOnMouseDown)
+                    this.handleOnMouseDown(e);
+            }.bind(this), false);
+
+            // Initialize background color
+            dataNode.style.backgroundColor = this.selectedBGColor;
 
             rowNode.appendChild(dataNode);
         }
@@ -55,59 +166,65 @@ EtchASketch.prototype.createSquareGrid = function(numPerSide = 16) {
     this.boardContainerNode.appendChild(boardNode);
 };
 
-/** Handles clicking the Clear button. */
+/** Clears the board and sets whole background-color to selected background color. */
 EtchASketch.prototype.handleClearBoard = function() {
-    // Get valid number of sides from user prompt
-    let numSides;
-    do {
-        numSides = +prompt('Enter number of sides (1-100): ');
-    } while (!Number.isInteger(numSides) || numSides < 1 || numSides > 100);
-
     // Remove all elements from container
     while (this.boardContainerNode.firstChild) 
         this.boardContainerNode.removeChild(this.boardContainerNode.firstChild);
 
     // Add new grid with prompted number of sides
-    this.createSquareGrid(numSides);
+    this.createSquareGrid(this.numPerSide);
+};
+
+/**
+ * Event handler to change background-color to selected foreground color.
+ * @param {Event} e 
+ */
+EtchASketch.prototype.addSelectedFGColor = function(e) {
+    e.target.style.backgroundColor = this.selectedFGColor;
+};
+
+/**
+ * Event handler to change background-color to selected background color.
+ * @param {Event} e 
+ */
+EtchASketch.prototype.erase = function(e) {
+    e.target.style.backgroundColor = this.selectedBGColor;
+};
+
+/**
+ * Event handler to change background-color to random color.
+ * @param {Event} e 
+ */
+EtchASketch.prototype.randomColor = function(e) {
+    e.target.style.backgroundColor = `hsl(${Math.floor(Math.random() * 360)}, 100%, 25%)`;
+};
+
+/**
+ * Makes the background-color of element darker or lighter by +/- rgb(10,10,10).
+ * @param {Element} element Event with target element to change background-color style
+ * @param {Boolean} isShadeDarker Shade darker if true, else shade lighter
+ */
+EtchASketch.prototype.shade = function(element, isShadeDarker) {
+    // Get adjusted color values of element background-color: 'rgb(100, 100, 100)' => 'rgb(90, 90, 90)]
+    const colorRGB = element.style.backgroundColor
+        .match(/\d+/g) // Get array of three color value numbers as strings for RGB values
+        .map(numStr => { // Adjust each color value
+            if (isShadeDarker) {
+                // Lower each color value by 10, limiting to 0. First convert from string to number.
+                return Math.max(+numStr - 10, 0);
+            } else {
+                // Increase each color value by 10, limiting to 255. First convert from string to number.
+                return Math.min(+numStr + 10, 255);
+            }
+        });
+
+    // Add darker color to background-color style
+    element.style.backgroundColor = `rgb(${colorRGB.join()})`;
 };
 
 (function() {
-    // Add black-white etch-a-sketch
-    const etchASketchBlackNWhite = new EtchASketch(
-        document.getElementById('black-n-white-etch-a-sketch'),
-        e => {
-            e.target.style.backgroundColor = 'black';
-        }
-    );
-
-    // Add random color etch-a-sketch
-    const etchASketchRandomColor = new EtchASketch(
-        document.getElementById('rand-color-etch-a-sketch'),
-        e => {
-            e.target.style.backgroundColor = `hsl(${Math.floor(Math.random() * 360)}, 100%, 25%)`;
-        }
-    );
-
-    // Add fade-to-black etch-a-sketch
-    // Add black-white etch-a-sketch
-    const etchASketchFadeToBlack = new EtchASketch(
-        document.getElementById('fade-to-black-etch-a-sketch'),
-        e => {
-            // Add 10% black to background-color
-            if (!e.target.dataset.hasOwnProperty('lightPercent')) {
-                e.target.dataset.lightPercent = 90;
-            } else {
-                const prevLightPercent = +e.target.dataset.lightPercent;
-                //  Return if already at 0%
-                if (prevLightPercent <= 0) return;
-                e.target.dataset.lightPercent = prevLightPercent - 10;
-            }
-
-            e.target.style.backgroundColor = `hsl(0, 0%, ${e.target.dataset.lightPercent}%)`;
-        }
-    );
-    
-    etchASketchBlackNWhite.init();
-    etchASketchRandomColor.init();
-    etchASketchFadeToBlack.init();
+    const etchASketch = new EtchASketch(document.querySelector('.etch-a-sketch-container'));
+    etchASketch.init();
+    window.etchASketch = etchASketch;
 })();
