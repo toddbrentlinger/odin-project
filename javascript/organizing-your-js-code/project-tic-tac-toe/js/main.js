937f1ed66c1796375ff1090e61853e7a261ca2a6
toddbrentlinger <toddbrentlinger@gmail.com>
2022-05-07 20:26:39 -0700

    Finish player edit form functionality in tic tac toe
    Allows user to change each Player's name, symbol, and color with the board
    and Player info updating with form submission.
    Added functionality to click outside of form to close OR click close
    button in top corner of form.
    Prevents any more squares from being selected when game has been won.
    Improves styling of player edit forms.

--- javascript/organizing-your-js-code/project-tic-tac-toe/js/main.js
+++ javascript/organizing-your-js-code/project-tic-tac-toe/js/main.js
@@ -5,32 +5,93 @@ const ticTacToe = (function() {
      * Factory function to create a Player object
      * @param {String} _name Name of the Player
      * @param {Number} _symbol Character code of symbol that represents the Player 
-     * @returns 
+     * @param {String} _color String representation of color to be used in CSS styling
+     * @param {Number} _key Number reprsenting player (ex. 1 is Player 1, 2 is Player 2). Compare with HTML element data attribute.
+     * @param {String} editElementId ID for element containing form for user to change Player properties
+     * @param {String} infoElementId ID for element displaying information about Player
+     * @returns {Object} player
      */
-    const playerFactory = function(_name, _symbol, _key) {
+    const playerFactory = function(_name, _symbol, _color, _key, editElementId, infoElementId) {
+        const _editElement = document.getElementById(editElementId);
+        const _infoElement = document.getElementById(infoElementId);
+
         const getName = () => _name;
         const setName = function(newName) {
             _name = newName;
+            _infoElement.querySelector('.player-name').textContent = _name;
         };
 
         const getSymbol = () => _symbol;
         const setSymbol = function(newSymbol) {
             _symbol = newSymbol;
+            _infoElement.querySelector('.player-symbol').textContent = _symbol;
+            gameBoard.refresh();
+        };
+
+        const getColor = () => _color;
+        const setColor = function(newColor) {
+            _color = newColor;
+            document.querySelector(':root').style.setProperty(`--player-${_key === 1 ? 'one' : 'two'}-color`, _color);
         };
 
         const getKey = () => _key;
 
-        const handleNameChange = function(e) {
+        const updateEditForm = function() {
 
         };
 
-        const handleSymbolChange = function(e) {
+        const handleEditFormSubmit = function(e) {
+            // Prevent default behavior of submitting form
+            e.preventDefault();
 
+            // Name
+            if (e.target.elements.name.value) {
+                setName(e.target.elements.name.value);
+            }
+            // Symbol
+            if (e.target.elements.symbol.value) {
+                setSymbol(e.target.elements.symbol.value);
+            }
+            // Color
+            if (e.target.elements.color.value !== getColor()) {
+                setColor(e.target.elements.color.value);
+            }
+
+            // Add hide class to edit element to remove from the window
+            _editElement.classList.add('hide');
         };
+
+        const handleEditFormClose = function() {
+            // Add hide class to edit element to remove from the window
+            _editElement.classList.add('hide');
+        };
+
+        const init = function () {
+            // Add event listener to edit form submit
+            _editElement.querySelector('form').addEventListener('submit', handleEditFormSubmit, false);
+
+            // Add event listener to close edit form
+            _editElement.querySelector('.close-player-edit-btn').addEventListener('click', handleEditFormClose, false);
+            // Also clicking outside of modal will close the edit form
+            _editElement.addEventListener('click', handleEditFormClose, false);
+            // Fix issue when clicking inside modal will also close the edit form
+            _editElement.firstElementChild.addEventListener('click', e => e.stopPropagation(), false);
+
+            // Update edit form to show existing Player values
+            updateEditForm();
+
+            // Add event listener to info element edit button to show Player edit form
+            _infoElement.querySelector('.edit-player-btn').addEventListener('click', () => {
+                // Remove 'hide' class from edit element to add to the window 
+                _editElement.classList.remove('hide');
+            }, false);
+        };
+        init();
 
         return {
             getName, setName,
             getSymbol, setSymbol,
+            getColor, setColor,
             getKey,
         };
     };
@@ -58,11 +119,20 @@ const ticTacToe = (function() {
             setPlayerSelect(null);
         };
 
+        const refresh = function() {
+            if (!_playerSelect) {
+                _squareElement.querySelector('span').textContent = '';
+            } else {
+                _squareElement.querySelector('span').textContent = _playerSelect.getSymbol();
+            }
+        };
+
         return {
             squareElement: _squareElement, 
             getPlayerSelect, setPlayerSelect,
             getBoardIndex,
             reset,
+            refresh,
         };
     };
 
@@ -90,7 +160,9 @@ const ticTacToe = (function() {
         };
 
         /**
-         * @returns {Object|null} Object with Player winner and the three Squares that make the line. Return null if NO winner. 
+         * @returns {Object|Number} gameResult Object with Player winner and the three Squares that make the line. Return 0 if game is still being played OR -1 for tie game.
+         * @returns {Player|null} gameResult.player Winning Player or null if no winner for tied games or games still being played.
+         * @returns {Square[]|null} gameResult.squares Array of Squares
          */
         const checkForWinner = function() {
             let square, playerToCompare;
@@ -115,15 +187,25 @@ const ticTacToe = (function() {
                 if (square.getPlayerSelect() !== playerToCompare) continue;
 
                 // If reach this point, win pattern has same Player in all three squares
-                console.log(`Win Condition Satisfied: ${WINNING_INDEX_PATTERNS[i]}\nWinning Player: ${playerToCompare.getName()}`);
-                return;
+                return {
+                    player: playerToCompare,
+                    squares: WINNING_INDEX_PATTERNS[i].map(squareIndex => getSquareByIndex(squareIndex)),
+                };
             }
 
             // If reach this point, no winner found.
             // Check if any more empty squares. If none, game is tied.
             if (_squares.every(square => square.getPlayerSelect() !== null)) {
-                console.log(`Tied Condition Satisfied: No more squares!`);
+                return -1;
             }
+
+            // If reach this point, no win or tie game
+            return 0;
+        };
+
+        /** Updates gameboard to reflect any changes in Player properties (symbol or color) */
+        const refresh = function() {
+            _squares.forEach(square => square.refresh());
         };
 
         const reset = function() {
@@ -146,15 +228,20 @@ const ticTacToe = (function() {
             init,
             checkForWinner,
             reset,
+            refresh,
         };
     })();
     
     const game = (function() {
-        const _playerOne = playerFactory('Player 1', 'X', 1);
-        const _playerTwo = playerFactory('Player 2', 'O', 2);
+        const _playerOne = playerFactory('Player 1', 'X', '#5271ff', 1, 'player-one-edit','player-one-info');
+        const _playerTwo = playerFactory('Player 2', 'O', '#ff5271', 2, 'player-two-edit', 'player-two-info');
         let bIsPlayerOneTurn = true;
+        let bIsGameOver = false;
 
         const handleSquareSelect = function(e, boardSquare) {
+            // Return if game is already over
+            if (bIsGameOver) return;
+
             // Return if square has already been selected by Player
             if (boardSquare.getPlayerSelect() !== null) return;
 
@@ -164,7 +251,19 @@ const ticTacToe = (function() {
             // Change turns to other Player
             bIsPlayerOneTurn = !bIsPlayerOneTurn;
 
-            gameBoard.checkForWinner();
+            const gameResults = gameBoard.checkForWinner();
+
+            // If game has winner
+            if (gameResults.hasOwnProperty('player')) {
+                bIsGameOver = true;
+                console.log(`Win Condition Squares: ${gameResults.squares.map(square => square.getBoardIndex())}\nWinning Player: ${gameResults.player.getName()}`);
+            }
+            // Else if game has tied
+            else if (gameResults === -1) {
+                bIsGameOver = true;
+                console.log(`Tied Condition Satisfied: No more squares!`);
+            }
+            // Else game continues
         };
 
         const init = function(boardElement, playerInfoElement) {
@@ -173,12 +272,13 @@ const ticTacToe = (function() {
 
         const reset = function() {
             bIsPlayerOneTurn = true;
+            bIsGameOver = false;
             gameBoard.reset();
         };
 
         const displayPlayerInfo = function() {
-            console.log(`Player One: ${_playerOne.getName()} - '${_playerOne.getSymbol()}'`);
-            console.log(`Player Two: ${_playerTwo.getName()} - '${_playerTwo.getSymbol()}'`);
+            console.log(`Player One: ${_playerOne.getName()} - '${_playerOne.getSymbol()}' - ${_playerOne.getColor()}`);
+            console.log(`Player Two: ${_playerTwo.getName()} - '${_playerTwo.getSymbol()}' - ${_playerTwo.getColor()}`);
         };
         return {
             init,
