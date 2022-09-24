fb7ffd6fb774450db76a1605d264bc65db4e7dcf
toddbrentlinger <toddbrentlinger@gmail.com>
2022-09-23 17:01:40 -0700

    Improve Working With APIs assignment.
    Adds "loading" message when a new GIF is being requested and loaded.
    Adds styling using SCSS.

--- javascript/asynchronous-javascript-and-apis/working-with-apis/main.js
+++ javascript/asynchronous-javascript-and-apis/working-with-apis/main.js
@@ -1,10 +1,14 @@
 'use strict';
 
 (function() {
-    const GIPHY_API_KEY = '<ENTER API CODE HERE>';
+    const GIPHY_API_KEY = '';
 
     function randomizeGIF() {
-        const img = document.getElementById('rand-cat-gif');
+        const img = document.querySelector('#rand-cat-gif-container .gif-img');
+        const loadingMsg = document.querySelector('#rand-cat-gif-container .loading-msg');
+
+        loadingMsg.classList.remove('hidden');
+
         fetch('https://api.giphy.com/v1/gifs/translate?api_key=' + GIPHY_API_KEY + '&s=cats', { mode: 'cors' })
             .then((response) => {
                 if (!response.ok) {
@@ -21,7 +25,11 @@
     }
 
     function searchGIF(searchText) {
-        const img = document.getElementById('search-gif');
+        const img = document.querySelector('#search-gif-container .gif-img');
+        const loadingMsg = document.querySelector('#search-gif-container .loading-msg');
+
+        loadingMsg.classList.remove('hidden');
+
         fetch('https://api.giphy.com/v1/gifs/translate?api_key=' + GIPHY_API_KEY + '&s=' + searchText, { mode: 'cors' })
             .then((response) => {
                 if (!response.ok) {
@@ -47,5 +55,13 @@
             const searchText = e.target.elements.namedItem('search').value;
             searchGIF(searchText);
             console.log(searchText);
+        });
+
+    document.querySelectorAll('.gif-img')
+        .forEach((gifImg) => {
+            gifImg.addEventListener('load', (e) => {
+                e.currentTarget.parentElement.querySelector('.loading-msg')
+                    .classList.add('hidden');
+            });
         });
 })();
